4783388e83afdd7af27c0fb1fe544fcc2cce2cd4
coding-fantastic <62503960+coding-fantastic@users.noreply.github.com>
2020-04-12 23:01:27 +0300

    Login
    admin and user can share the page

---
+++ Login.php
@@ -0,0 +1,93 @@
+<?php
+session_start();//session starts here
+
+?>
+
+
+
+<html>
+<head lang="en">
+    <meta charset="UTF-8">
+    <link type="text/css" rel="stylesheet" href="bootstrap-3.2.0-dist\css\bootstrap.css">
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- Optional theme -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    <title>Login</title>
+</head>
+<style>
+    .login-panel {
+        margin-top: 150px;
+      }
+
+</style>
+
+<body>
+
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-4 col-md-offset-4">
+            <div class="login-panel panel panel-success">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Sign In</h3>
+                </div>
+                <div class="panel-body">
+                    <form role="form" method="post" action="Login.php">
+                        <fieldset>
+                            <div class="form-group"  >
+                                <input class="form-control" placeholder="E-mail" name="email" type="email" autofocus>
+                            </div>
+                            <div class="form-group">
+                                <input class="form-control" placeholder="Password" name="pass" type="password" value="">
+                            </div>
+
+
+                                <input class="btn btn-lg btn-success btn-block" type="submit" value="login" name="login" >
+
+                            <!-- Change this to a button or input when using this as a form -->
+                          <!--  <a href="index.html" class="btn btn-lg btn-success btn-block">Login</a> -->
+                        </fieldset>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+</body>
+
+</html>
+
+<?php
+
+include("database/db_connection.php");
+
+if(isset($_POST['login']))
+{
+    $user_email=$_POST['email'];
+    $user_pass=$_POST['pass'];
+
+    $check_user="select * from users WHERE user_email='$user_email'AND user_pass='$user_pass'";
+
+    $run=mysqli_query($dbcon,$check_user);
+
+    if(mysqli_num_rows($run))
+    {
+        echo "<script>window.open('Welcome.php','_self')</script>";
+
+        $_SESSION['email']=$user_email;//here session is used and value of $user_email store in $_SESSION.
+
+    }
+    else
+    {
+      echo "<script>alert('Email or password is incorrect!')</script>";
+    }
+}
+?>
