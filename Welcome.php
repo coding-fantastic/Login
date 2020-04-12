50f6712300bb981fb4481ffb14f2a27700c4d805
coding-fantastic <62503960+coding-fantastic@users.noreply.github.com>
2020-04-12 23:24:55 +0300

    welcome page

---
+++ Welcome.php
@@ -0,0 +1,41 @@
+<?php
+session_start();
+
+if(!$_SESSION['email'])
+{
+
+    header("Location: Login.php");//redirect to login page to secure the welcome page without login access.
+}
+
+?>
+
+<html>
+<head>
+
+  <!-- Latest compiled and minified CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+  <!-- Optional theme -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+
+  <!-- Latest compiled and minified JavaScript -->
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+    <title>
+        Registration
+    </title>
+</head>
+
+<body>
+<h1>Welcome</h1><br>
+<?php
+echo $_SESSION['email'];
+?>
+
+
+<h1><a href="Logout.php">Logout here</a> </h1>
+
+
+</body>
+
+</html>
